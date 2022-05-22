9aad201b67055ce2f0fe04810e4afec8b5a2a47a
Louise Wong <lw17122@bristol.ac.uk>
2022-05-22 13:16:53 +0100

    initialised react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>It is working</h1>
+    </div>
+  )
+}
+
+export default App
